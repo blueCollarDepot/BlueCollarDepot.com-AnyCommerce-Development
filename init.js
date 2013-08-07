f72d14e4d0f5a752587cb626d5e91786fe05a5b0
clintoncontreras <ClintonC@zoovy.com>
2013-08-07 15:55:57 -0700

    Corrected minimized arrows for customizer sections

--- init.js
+++ init.js
@@ -53,10 +53,10 @@ app.rq.push(['templateFunction','productTemplate','onCompletes',function(P) {
 		
 		$("<div class='customizerMenu'>"
 			+ "<div class='customizerCatCont' onClick='app.ext.pogs_blueCollar.a.toggleHideShowStriping()'>"
-			+ "<label>Reflective Striping</label><label class='floatRight customizerSectionClosedStripe displayNone'>&#9664;</label><label class='floatRight customizerSectionOpenStripe'>&#9660;</label></div>"
+			+ "<label>Reflective Striping</label><label class='floatRight customizerSectionClosedStripe displayNone'>&#9654;</label><label class='floatRight customizerSectionOpenStripe'>&#9660;</label></div>"
 			+ "<div class='stipeCustomizer'></div>"
 			+ "<div class='customizerCatCont' onClick='app.ext.pogs_blueCollar.a.toggleHideShowEmbroidery()'>"
-			+ "<label>Embrodiery/Logos</label><label class='floatRight customizerSectionClosedEmbroid displayNone'>&#9664;</label><label class='floatRight customizerSectionOpenEmbroid'>&#9660;</label></div>"
+			+ "<label>Embrodiery/Logos</label><label class='floatRight customizerSectionClosedEmbroid displayNone'>&#9654;</label><label class='floatRight customizerSectionOpenEmbroid'>&#9660;</label></div>"
 			+ "   <div class='embroideryCustomizer'>"
 			+ "     <div class='embroideryCont'>"
 			+ "			<div class='floatLeft clearLeft'>"
